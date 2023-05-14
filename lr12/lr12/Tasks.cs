d2a43feb4bfae453a90a149cd4d765fa77ba99b9
averagge <aleksejporfirev15@gmail.com>
2023-05-14 13:14:47 +0300

    Создал классы пользователей и задач

---
+++ lr12/lr12/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr12
+{
+    public class Tasks
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Ststus { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
